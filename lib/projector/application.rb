b0f3d40e8cc6ea105b3fff829bd29b7104c0f940
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-10-26 22:25:57 +0000

    Copy arg parsing from this article to get some sort of arg parsing working: https://practicingruby.com/articles/building-unix-style-command-line-applications

--- lib/projector/application.rb
+++ lib/projector/application.rb
@@ -2,9 +2,27 @@ module Projector
   class Application
     def initialize(argv)
       puts "Hello world!"
+      @params, @files = parse_options(argv)
     end
 
     def run
+      puts "Running now!"
+    end
+
+    def parse_options(argv)
+      params = {}
+      parser = OptionParser.new
+
+      parser.on("-n") { params[:line_numbering_style] ||= :all_lines         }
+      parser.on("-b") { params[:line_numbering_style]   = :significant_lines }
+      parser.on("-s") { params[:squeeze_extra_newlines] = true               }
+
+      files = parser.parse(argv)
+
+      puts params
+      puts files
+
+      [params, files]
     end
   end
 end
