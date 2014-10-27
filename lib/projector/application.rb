209db7e4894d04791c249f848c3f6ce245630ad4
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2014-10-27 23:44:30 +0000

    Extract method for initialising the DB

--- lib/projector/application.rb
+++ lib/projector/application.rb
@@ -1,40 +1,41 @@
+require "sqlite3"
+
 require_relative "task_options"
 
 module Projector
   class Application
     def initialize(argv)
       @params = TaskOptions.new(argv)
+
+      initialize_db()
     end
 
     def run
-      require "sqlite3"
+        # Insert a new task
+        {
+          @params.task => 0
+        }.each do |pair|
+          @db.execute "insert into tasks values ( ?, ? )", pair
+        end
 
-        # Open a database
-        db = SQLite3::Database.new "db/tasks.db"
+        # Find a few rows
+        @db.execute( "select * from tasks" ) do |row|
+          p row
+        end
+    end
 
-        # Create a database
-        rows = db.execute <<-SQL
+    private
+    def initialize_db
+        # Open the database
+        @db = SQLite3::Database.new "db/tasks.db"
+
+        # Create the tasks table if it doesn't already exist
+        rows = @db.execute <<-SQL
           create table if not exists tasks (
             name varchar,
             completion smallint
           );
         SQL
-
-        # Insert a new task
-        {
-          @params.task => 0
-        }.each do |pair|
-          db.execute "insert into tasks values ( ?, ? )", pair
-        end
-
-        # Execute inserts with parameter markers
-        # db.execute("INSERT INTO students (name, email, grade, blog)
-        #             VALUES (?, ?, ?, ?)", [@name, @email, @grade, @blog])
-
-        # Find a few rows
-        db.execute( "select * from tasks" ) do |row|
-          p row
-        end
     end
   end
 end
