162c31443f59f29378d0d008ee0d666c185a6f76
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-05-03 00:58:00 +0100

    Delete empty test setup

--- test/test_projector.rb
+++ test/test_projector.rb
@@ -2,10 +2,6 @@ require "minitest/autorun"
 require "projector"
 
 class TestProjector < Minitest::Test
-  def setup
-
-  end
-
   def test_that_basic_test_can_run
     assert_output("Hello kitty\n") do
       Projector::start(["hello", "kitty"])
