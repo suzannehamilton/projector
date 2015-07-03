345f1088700650ef080e7f6c0270127902da8861
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-07-03 22:38:53 +0100

    Delete now-redundant dummy command-line task

--- lib/projector.rb
+++ lib/projector.rb
@@ -1,10 +1,6 @@
 require "thor"
 
 class Projector < Thor
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
-  end
 
   desc "list", "list unfinihsed tasks"
   def list
--- test/test_projector.rb
+++ test/test_projector.rb
@@ -3,13 +3,6 @@ require "projector"
 
 class TestProjector < Minitest::Test
 
-  # TODO SMH: Remove 'hello' task once there are real examples of calls which use arguments
-  def test_basic_command_line_call_picks_up_arguments
-    assert_output("Hello kitty\n") do
-      Projector::start(["hello", "kitty"])
-    end
-  end
-
   def test_listing_empty_task_list_identifies_that_no_tasks_are_available
     assert_output("Nothing left to do!\n") do
       Projector::start(["list"])
