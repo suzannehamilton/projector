fdf4d9c5e9f17552413bad2df45d6055c9e4fb6d
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-07-10 21:20:21 +0100

    Start to use mock DB in tests of application

--- test/test_application.rb
+++ test/test_application.rb
@@ -4,16 +4,17 @@ require "application"
 class TestApplication < Minitest::Test
 
   def setup
-    # TODO: Replace with mock
-    database = Database.new
-    @application = Application.new(database)
+    @database = MiniTest::Mock.new
+    @application = Application.new(@database)
   end
 
   def test_listing_empty_task_list_identifies_that_no_tasks_are_available
+    @database.expect(:list, [])
     assert_equal("Nothing left to do!", @application.list)
   end
 
   def test_adding_a_task_to_list_returns_task_name
+    # TODO: Verify that mock is called
     assert_equal("Added 'Some task'", @application.add("Some task"))
   end
 end
