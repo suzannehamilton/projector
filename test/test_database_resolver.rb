701a13c8f728a51c5e9a901b42bce1b6cb6cd546
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-07-12 23:59:53 +0100

    Test that a database can be resolved if this is the first time the application is being run

--- test/test_database_resolver.rb
+++ test/test_database_resolver.rb
@@ -3,7 +3,27 @@ require "minitest/autorun"
 
 class TestDatabaseResolver < Minitest::Test
 
+  TEST_DB_FILE = "db/tasks.db"
+
+  def setup
+    File::delete(TEST_DB_FILE) if File::exist?(TEST_DB_FILE)
+  end
+
+  # TODO: Determine how to get after_tests to run, since it looks like they aren't being hit
+  def after_tests
+    File::delete(TEST_DB_FILE)
+  end
+
   def test_returns_empty_database
     assert_empty(DatabaseResolver.new.get_database.list)
   end
+
+  def test_creates_database_file
+    refute(File::file?(TEST_DB_FILE), "Test setup is incorrect: a DB file exists at #{TEST_DB_FILE} but none was expected")
+    DatabaseResolver.new.get_database.list
+    assert(File::file?(TEST_DB_FILE))
+  end
+
+  # TODO: Test that an existing DB file is picked up correctly
+  # TODO: Test that running 'list' without the test env variable does not fail
 end
