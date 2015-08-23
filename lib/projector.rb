a8c908fecf31a6d467fd38bdac943fa8bcc24507
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-08-23 22:54:35 +0100

    Replace 'puts' with 'say' in main Thor subclass, which is how you're supposed to output text to the console in Thor

--- lib/projector.rb
+++ lib/projector.rb
@@ -14,16 +14,16 @@ class Projector < Thor
 
   desc "list", "list unfinished tasks"
   def list
-    puts @application.list
+    say @application.list
   end
 
   desc "add TASK", "add a new task named TASK"
   def add(name)
-    puts @application.add(name)
+    say @application.add(name)
   end
 
   desc "complete TASK_NUMBER", "mark task labelled TASK_NUMBER as complete"
   def complete(task_number)
-    puts @application.complete(task_number)
+    say @application.complete(task_number)
   end
 end
