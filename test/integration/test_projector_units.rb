24578fd2e18f3126b1caf34921c204a1fa5920b3
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-09-30 23:45:15 +0100

    Delete already-handled TODOs

--- test/integration/test_projector_units.rb
+++ test/integration/test_projector_units.rb
@@ -28,7 +28,5 @@ class TestProjectorUnits < TestProjector
     end
   end
 
-  # TODO: Test switching back to percent when max in new units is not 100
   # TODO: Test updating size when switching units
-  # TODO: Test updating units when size is not 100
 end
