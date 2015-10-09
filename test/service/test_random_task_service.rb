5b8ea75b6a29bdb41410acd2c9576c83f99ac368
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2015-10-09 22:53:07 +0100

    Add basic test that the random task is one of several tasks to choose from

--- test/service/test_random_task_service.rb
+++ test/service/test_random_task_service.rb
@@ -17,5 +17,17 @@ class TestRandomTaskService < Minitest::Test
     assert_equal(task, @random_task_service.get_random_task([task]))
   end
 
+  def test_random_task_is_one_of_several_tasks
+    task1 = "some task 1"
+    task2 = "some task 2"
+    task3 = "some task 3"
+
+    all_tasks = [task1, task2, task3]
+
+    random_task = @random_task_service.get_random_task(all_tasks)
+
+    assert all_tasks.include? random_task
+  end
+
   # TODO: Test random selection
 end
